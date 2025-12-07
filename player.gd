9a75fe8ace3ccaf47cae97d0ac900137b3561b58
ncssm-mor-NyE9DoBlxs3M <245083691+ncssm-mor-NyE9DoBlxs3M@users.noreply.github.com>
2025-12-07 15:33:16 -0500

    feat: jump?

--- player.gd
+++ player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-const speed_cap: int = 1000
+const speed_cap: float = 1000.
 const accel_rate: float = 500.
 const decel_rate: float = 900.
-
+const min_jump_strength: float = 150.
+const max_jump_strength: float = 300.
 const coyote_time: float = .2
-
+const jump_conversion: float = .5
 var current_coyote_time: float
 
 func _process(delta) -> void:
@@ -13,6 +14,10 @@ func _process(delta) -> void:
 	if is_on_floor():
 		velocity = velocity.move_toward(input*speed_cap, (accel_rate if (input.dot(velocity) > 0) else decel_rate)*delta)
 		current_coyote_time = coyote_time
+		if Input.is_action_just_pressed("jump"):
+			var jump_strength = min(jump_conversion*abs(velocity.x), max_jump_strength-min_jump_strength)
+			velocity.y -= jump_strength+min_jump_strength
+			velocity.x -= jump_strength*sign(velocity.x)
 	else:
 		current_coyote_time = max(0, current_coyote_time-delta)
 		velocity.y += get_gravity().y * delta
