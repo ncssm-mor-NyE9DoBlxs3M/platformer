7655855e71d5492aa9616f4136fcf85c6e01b8aa
ncssm-mor-NyE9DoBlxs3M <245083691+ncssm-mor-NyE9DoBlxs3M@users.noreply.github.com>
2026-02-07 14:40:48 -0500

    checkpoint disappears on use

--- checkpoint.gd
+++ checkpoint.gd
@@ -1,4 +1,9 @@
 class_name Checkpoint extends Area2D
 
 func activate() -> void:
+	set_deferred("monitorable", false)
 	$Sound.play()
+	var tween := get_tree().create_tween()
+	tween.tween_property($Sprite2D, "modulate", Color.TRANSPARENT, 1.)
+	await tween.finished
+	queue_free()
