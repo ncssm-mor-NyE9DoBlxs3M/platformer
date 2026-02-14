2720bba8da27070b9260224699cc0e344f6c144f
ncssm-mor-NyE9DoBlxs3M <245083691+ncssm-mor-NyE9DoBlxs3M@users.noreply.github.com>
2026-02-14 16:53:14 -0500

    main menu ish

--- main.gd
+++ main.gd
@@ -1,11 +1,24 @@
 extends Control
 
 @export var start_level: PackedScene
-@onready var level: Level = start_level.instantiate()
+var level: Level
+
+func _ready() -> void:
+	$UI/MainMenu/Menu/Play.grab_focus()
+
+func _on_button_hover(node: NodePath) -> void:
+	get_node(node).grab_focus()
 
 func _ready() -> void:
 	$Game.add_child(level)
 
 func _process(_delta: float) -> void:
 	if level:
-		$UI/Timer.text = "%1d:%2.2f" % [level.timer.time_left/60., fmod(level.timer.time_left,60.)]
+		$UI/Timer.text = "%1d:%05.2f" % [level.timer.time_left/60., fmod(level.timer.time_left,60.)]
+
+func _on_play_pressed() -> void:
+	$UI/MainMenu.hide()
+	level = load_level(start_level)
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
