a2467662c234f0a916e803ec9ea26b714a4eb089
ncssm-mor-NyE9DoBlxs3M <245083691+ncssm-mor-NyE9DoBlxs3M@users.noreply.github.com>
2026-02-14 16:53:14 -0500

    main menu ish

--- main.gd
+++ main.gd
@@ -1,7 +1,13 @@
 extends Control
 
 @export var start_level: PackedScene
-@onready var level: Level = load_level(start_level)
+var level: Level
+
+func _ready() -> void:
+	$UI/MainMenu/Menu/Play.grab_focus()
+
+func _on_button_hover(node: NodePath) -> void:
+	get_node(node).grab_focus()
 
 func load_level(scene: PackedScene) -> Level:
 	var new_level = scene.instantiate()
@@ -15,3 +21,10 @@ func load_level(scene: PackedScene) -> Level:
 func _process(_delta: float) -> void:
 	if level:
 		$UI/Timer.text = "%1d:%05.2f" % [level.timer.time_left/60., fmod(level.timer.time_left,60.)]
+
+func _on_play_pressed() -> void:
+	$UI/MainMenu.hide()
+	level = load_level(start_level)
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
